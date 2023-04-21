becaa24b7c9bfc2a7bf1ffdf0df7b410cdc66a7f
MinMag <ocon0574@umn.edu>
2023-04-20 20:08:42 -0500

    updated main file to reflect current code

--- gardenbot.c
+++ gardenbot.c
@@ -1,67 +1,178 @@
-/*
- * File:   newmainXC16.c
- * Author: magnu
- *
- * Created on April 7, 2023, 2:39 PM
- */
-#include <p24FJ64GA002.h>
-
 #include "xc.h"
 #include "WDT_lib.h"
-// CW1: FLASH CONFIGURATION WORD 1 (see PIC24 Family Reference Manual 24.1)
-#pragma config ICS = PGx1          // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
-#pragma config FWDTEN = OFF        // Watchdog Timer Enable (Watchdog Timer is disabled)
-#pragma config GWRP = OFF          // General Code Segment Write Protect (Writes to program memory are allowed)
-#pragma config GCP = OFF           // General Code Segment Code Protect (Code protection is disabled)
-#pragma config JTAGEN = OFF        // JTAG Port Enable (JTAG port is disabled)
+#include <p24FJ64GA002.h>
 
-
-
+#pragma config ICS = PGx1
+#pragma config FWDTEN = OFF    
+#pragma config GWRP = OFF   	 
+#pragma config GCP = OFF    	 
+#pragma config JTAGEN = OFF	 
+#pragma config I2C1SEL = PRI	 
+#pragma config IOL1WAY = OFF 	 
+#pragma config OSCIOFNC = ON 	 
+#pragma config FCKSM = CSECME    
+#pragma config FNOSC = FRCPLL
 #pragma config FWPSA = PR128       // Configures Prescalar for WDT, in this case 
 #pragma config WDTPS = PS32768     // Configures Postscalar for WDT
                                    // WDT period (ms) = (FWPSA/32) * WDTPS
 
+#define BUFSIZE 1024
+#define NUMSAMPLES 30
+#define MOISTURETHRESHOLD 350 //ADC1
+#define WATERLEVELTHRESHOLD 350 //ADC2
 
-// CW2: FLASH CONFIGURATION WORD 2 (see PIC24 Family Reference Manual 24.1)
-#pragma config I2C1SEL = PRI       // I2C1 Pin Location Select (Use default SCL1/SDA1 pins)
-#pragma config IOL1WAY = OFF       // IOLOCK Protection (IOLOCK may be changed via unlocking seq)
-#pragma config OSCIOFNC = ON       // Primary Oscillator I/O Function (CLKO/RC15 functions as I/O pin)
-#pragma config FCKSM = CSECME      // Clock Switching and Monitor (Clock switching is enabled, 
-                                       // Fail-Safe Clock Monitor is enabled)
-#pragma config FNOSC = FRCPLL      // Oscillator Select (Fast RC Oscillator with PLL module (FRCPLL))
+unsigned int adc_buffer1[BUFSIZE];
+unsigned int adc_buffer2[BUFSIZE];
+volatile int buffer_index = 0;
+
+volatile int state = 0;
 
 void delay_ms(unsigned int ms) {
-    while (ms-- > 0) {
-        asm("repeat #15998");
-        asm("nop");
-    }
+	while (ms-- > 0) {
+    	asm("repeat #15998");
+    	asm("nop");
+	}
+}
+void pic24_setup(void){
+	_RCDIV = 0;
+	AD1PCFG = 0x7FFE;
+	AD1PCFGbits.PCFG0 = 0; // set AN0 to analog
+	AD1PCFGbits.PCFG1 = 0; // set AN1 to analog
+	TRISBbits.TRISB12 = 0;   // Set RB12 as output
+	TRISAbits.TRISA0 = 1;  // Set RA0/AN0 to Input
+	TRISAbits.TRISA1 = 1; // Set RA1/AN1 to Input
+}
+unsigned int getAvg1(){
+	unsigned long sum = 0;
+    	int index = buffer_index;
+    	for (int i = 0; i < NUMSAMPLES; i++) {
+        	sum += adc_buffer1[index--]; // use decrementing index
+        	if (index < 0) { // check for buffer underflow
+            	index = BUFSIZE - 1;
+        	}
+    	}
+    	unsigned int average = sum / NUMSAMPLES;
+    	return average;
+}
+unsigned int getAvg2(){
+	unsigned long sum = 0;
+    	int index = buffer_index;
+    	for (int i = 0; i < NUMSAMPLES; i++) {
+        	sum += adc_buffer2[index--]; // use decrementing index
+        	if (index < 0) { // check for buffer underflow
+            	index = BUFSIZE - 1;
+        	}
+    	}
+    	unsigned int average = sum / NUMSAMPLES;
+    	return average;
+}
+void putVal1(unsigned int ADCvalue){
+	adc_buffer1[buffer_index++] = ADCvalue;
+	if(buffer_index >= BUFSIZE){
+    	buffer_index = 0;
+	}
+}
+void putVal2(unsigned int ADCvalue){
+	adc_buffer2[buffer_index++] = ADCvalue;
+	if(buffer_index >= BUFSIZE){
+    	buffer_index = 0;
+	}
+}
+void initBuffer(){
+	int i;
+	for(i=0; i < BUFSIZE; i++){
+    	adc_buffer1[i] = 0;
+    	adc_buffer2[i] = 0;
+	}
+}
+void adc_init1(){
+	AD1CON2bits.VCFG = 0b000; // VDD max (3.3V) and VSS min (0V)
+    
+	AD1CON3bits.ADCS = 1; // TAD >= 75 ns (Tcy = 62.5 ns)
+	AD1CON1bits.SSRC = 2; // Sample on timer3 events
+	AD1CON3bits.SAMC = 1; // At least 1 auto sample time bit
+	AD1CON1bits.FORM = 0; // Unsigned int
+	AD1CON1bits.ASAM = 1; // Reference Manual
+	AD1CON2bits.SMPI = 0; // Reference Manual
+	AD1CON1bits.ADON = 1; // Turn on ADC
+    
+	_AD1IF = 0;
+	_AD1IE = 1; // Enable ADC interrupt
+    
+	TMR3 = 0;  // Set up timer3
+	T3CON = 0;
+	T3CONbits.TCKPS = 0b10;// 1:64
+	PR3 = 15624;
+	T3CONbits.TON = 1; // 16 times per second
+}
+void initPushButton(void) {
+	__builtin_write_OSCCONL(OSCCON & 0xbf); // unlock PPS
+	RPINR7bits.IC1R = 8; // Use Pin RP8 = "8", for Input Capture 1 (Table 10-2)
+	__builtin_write_OSCCONL(OSCCON | 0x40); // lock   PPS
+	T2CON = 0;
+	T2CONbits.TCKPS = 0b11;
+	PR2 = 62499; // 1s cycle
+	TMR2 = 0;
+	IEC0bits.T2IE = 1; // Enable T2 Interrupt
+	TRISBbits.TRISB8 = 1; // Pin 17 is input
+	CNPU2bits.CN22PUE = 1; //Pull-up Resistor Enable
+	_T2IF = 0;
+	T2CONbits.TON = 0;
+	IC1CON = 0;
+	IC1CONbits.ICTMR = 1;
+	IC1CONbits.ICM = 0b010; // Capture every fall
+	IC1BUF = 0;
+	IEC0bits.IC1IE = 1; // Input capture 1 Interrupt Enable
+}
+void __attribute__((interrupt, auto_psv)) _ADC1Interrupt(void){
+	// Call putVal() on ADC1BUF0
+	putVal1(ADC1BUF0);
+	putVal2(ADC1BUF1);
+	// Reset the ADC interrupt flag
+	IFS0bits.AD1IF = 0;
+}
+void __attribute__((interrupt, auto_psv)) _T2Interrupt() { // rollover for T2 ISR
+	_T2IF = 0;
+	TMR2 = 0;
+}
+void __attribute__((interrupt, auto_psv)) _IC1Interrupt() { // Detect click ISR
+	_IC1IF = 0;
+	pumpEnable();
+	}   
+void pumpEnable(){
+    	LATBbits.LATB12 = 1;	// Set RB2 high
+    	delay_ms(1000);
+    	LATBbits.LATB12 = 0;	// Set RB2 low
+    	state = 0;
 }
 
-int main(void) {
-    _RCDIV = 2;
-    AD1PCFG = 0xffff;
-    TRISBbits.TRISB6 = 0;    
+void loop() {
+	while (1) {
+    	while (IFS0bits.T1IF == 0);
+    	if(getAvg2() < WATERLEVELTHRESHOLD){
+        	//Buzzer
+        	//Buzz more
+    	}
+    	else{
+        	if(getAvg1() < MOISTURETHRESHOLD){
+            	pumpEnable(); //Water soil
+            	//Wait 5 minutes
+        	}
+        	else{
+            	//Go to sleep
+        	}
+    	}
+   	 
+    	IFS0bits.T1IF = 0;
+	}
+}
+int main() {
+	initBuffer();
+	pic24_setup();
+	initPushButton();
+	adc_init1();
+	T2CONbits.TON = 1;
     
-    while(1) {
-        LATBbits.LATB6 = 0;
-        delay_ms(1000);
-        LATBbits.LATB6 = 1;
-        
-//        ClrWdt();
-        int x = 0;
-        //Below is basic structure to create a desired sleep longer than one WDT period
-        while(x < 2) { //Sleep for approximately 2 * WDTPeriod seconds w/ the LED on
-            RCONbits.SWDTEN = 1;
-            Sleep();
-            RCONbits.SLEEP  = 0;
-            RCONbits.WDTO = 0;
-            RCONbits.SWDTEN = 0;
-            x++;
-        }
-        sleepNperiods(2);
-    }
+	loop();
     
-    
-    
-    return 0;
 }
