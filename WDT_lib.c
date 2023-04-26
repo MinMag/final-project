c59dc75a61cefbdfb6c7f613cfa4f655e2659357
MinMag <ocon0574@umn.edu>
2023-04-26 10:20:34 -0500

    WDT comments

--- WDT_lib.c
+++ WDT_lib.c
@@ -9,7 +9,13 @@
 #include "xc.h"
 #include "WDT_lib.h"
 
-
+/* This function allows the PIC to go to sleep for periods longer than 1 
+ * WDT period. This is implemented by counting a variable up to N, increasing
+ * time the PIC wakes from sleep. Total sleep time will be N * WDT period
+ * 
+ * Arguments: int N - the number of desired periods to sleep for
+ * Returns: nothing
+ */
 void sleepNperiods(int N) {
     int t;
     for(t = 0; t < N; t++) {
