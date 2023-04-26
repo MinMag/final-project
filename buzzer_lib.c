10d891e05f93cf83ed887b6250ae1f433b3ca53a
MinMag <ocon0574@umn.edu>
2023-04-26 10:31:24 -0500

    Update buzzer_lib.c

--- buzzer_lib.c
+++ buzzer_lib.c
@@ -6,7 +6,6 @@
 #define WATERLEVELTHRESHOLD (1.65/3.3)*1023 //ADC2, Threshold of 1.65
 void buzzerEnable(){
     LATBbits.LATB6 = 1; //sets RB6 as high
-
 }
 
 void buzzerDisable(volatile int* overflow){
